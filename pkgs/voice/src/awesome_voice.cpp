8722bc48eefe5db4c3b9b405ebe87499adc0c900
Razik <raz@RAZ-LAPTOP.(none)>
2012-02-28 15:29:47 -0600

    Stylistic changes

--- pkgs/voice/src/awesome_voice.cpp
+++ pkgs/voice/src/awesome_voice.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 void say_heading(int robot_x, int robot_y, int robot_heading, int obj_x, int obj_y) {
     stringstream command_string;
-
     command_string << "espeak -v mb-us1 \"The object is " << (((int)(atan2(obj_y - robot_y, obj_x - robot_x)*180/M_PI+360)%360)>robot_heading ? ((int)(atan2(obj_y - robot_y, obj_x - robot_x)*180/M_PI+360)%360)-robot_heading : robot_heading-((int)(atan2(obj_y - robot_y, obj_x - robot_x)*180/M_PI+360)%360)) << "degrees to the " << (((int)(atan2(obj_y - robot_y, obj_x - robot_x)*180/M_PI+360)%360)>robot_heading ? "left" : "right") << " and " << (int) sqrt(pow(obj_x-robot_x, 2) + pow(obj_y-robot_y, 2)) << " units away.\"";
     system(command_string.str().c_str());
 }
